554c1968d70f79c030e50b7bebb52cf5511a6883
MarkChburg <1766163080@qq.com>
2018-06-25 00:22:32 +0800

    增加BeanMapping相關field

--- src/main/java/com/github/wjt/poitrans/ExcelCrawler.java
+++ src/main/java/com/github/wjt/poitrans/ExcelCrawler.java
@@ -2,6 +2,9 @@ package com.github.wjt.poitrans;
 
 import com.github.wjt.poitrans.connection.Connection;
 import com.github.wjt.poitrans.connection.ExcelConnection;
+import com.github.wjt.poitrans.mapping.BeanMapping;
+import com.github.wjt.poitrans.mapping.DefaultBeanMapping;
+import com.github.wjt.poitrans.parser.BaseSQLInfo;
 import com.github.wjt.poitrans.parser.DefaultParserDelegate;
 import com.github.wjt.poitrans.parser.SQLParser;
 import com.github.wjt.poitrans.statement.ExcelStatement;
@@ -9,9 +12,10 @@ import com.github.wjt.poitrans.statement.ResultMap;
 import com.github.wjt.poitrans.statement.Statement;
 
 import java.io.IOException;
+import java.util.Set;
 
 // main entrance
-public class ExcelCrawler implements Runnable{
+public class ExcelCrawler {
 
     private Connection connection;
 
@@ -19,7 +23,11 @@ public class ExcelCrawler implements Runnable{
 
     private SQLParser sqlParser = DefaultParserDelegate.getParser();
 
-    // private BeanMapping beanMapping;
+    private BeanMapping beanMapping = DefaultBeanMapping.getMapping();
+
+    private BaseSQLInfo baseSQLInfo;
+
+    private Class retClass;
 
     private ExcelCrawler() {
 
@@ -27,6 +35,15 @@ public class ExcelCrawler implements Runnable{
 
     public static ExcelCrawler build() {
         return new ExcelCrawler();
+    }
+
+    public ExcelCrawler beanMapping(BeanMapping beanMapping) {
+        return addBeanMapping(beanMapping);
+    }
+
+    private ExcelCrawler addBeanMapping(BeanMapping beanMapping) {
+        this.beanMapping = beanMapping;
+        return this;
     }
 
     public ExcelCrawler connection(String fileName) {
@@ -44,19 +61,44 @@ public class ExcelCrawler implements Runnable{
 
     private ExcelCrawler addStatement(SQLHolder sqlHolder) {
         this.statement = new ExcelStatement(sqlHolder, connection);
+        baseSQLInfo = sqlParser.parse(sqlHolder);
+        return this;
+    }
+
+    public <T> ExcelCrawler setMappingBean(Class<T> clazz) {
+        return addMappingBean(clazz);
+    }
+
+    private <T> ExcelCrawler addMappingBean(Class<T> clazz) {
+        this.retClass = clazz;
         return this;
     }
 
     public static void main(String[] args) {
-        ExcelCrawler.build().statement(new SQLHolder("select * from Sheet1")).run();
+        try {
+            ExcelCrawler.build().statement(new SQLHolder("select * from test1")).setMappingBean(TestDemo.class).execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void run() {
+
+    public <T> T execute() throws Exception {
+        T bean = null;
         try {
-            statement.execute();
+            Set<ResultMap<String, String>> resultMaps = statement.queryExecute();
+            for (ResultMap<String, String> resultMap : resultMaps) {
+                try {
+                    bean = beanMapping.getBean(resultMap, baseSQLInfo, (Class<T>) retClass);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return bean;
     }
 }
